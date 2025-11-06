31abb79fe2acd5b7f82749ae9512defaf36301ff
Lawal Taofik <layomide81@gmail.com>
2025-11-06 10:34:32 +0100

    fix double footer

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Navbar from '@/components/Navbar'
-import Footer from '@/components/ui/Footer'
+// import Footer from '@/components/ui/Footer'
 import MainPage from '@/components/ui/UserDashboard/MainPage'
 import React from 'react'
 
@@ -8,7 +8,7 @@ function page() {
   return (
     <div>
       <MainPage/>
-      <Footer/>
+      {/* <Footer/> */}
     </div>
   );
 }
